a4e7fac5586f5bed614bd34e89019176f7b72b0a
piyuxx <piyushsharma3rules@gmail.com>
2023-03-03 16:41:07 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB-lLw3JUfv4drK_er2BXiVKaMGNysJpk",
+  authDomain: "contact-list-9616a.firebaseapp.com",
+  projectId: "contact-list-9616a",
+  storageBucket: "contact-list-9616a.appspot.com",
+  messagingSenderId: "1069084057511",
+  appId: "1:1069084057511:web:c3e93cd1ec7bcdb470a082"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { createStore } from 'redux';
+import contactReducer from './redux/reducers/contactReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux'
+
+const store = createStore(contactReducer, composeWithDevTools());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
